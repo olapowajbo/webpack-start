1474a504ffb0b99bb6d1d27a899e7355f26e06c3
Ola Powajbo <aleksandrapowajbo@wp.pl>
2020-06-21 07:38:07 +0200

    page Ola added

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/ola.html",
+      inject: true,
+      chunks: ["index",],
+      filename: "ola.html",
+    }),
   ],
 };
